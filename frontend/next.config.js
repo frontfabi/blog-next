7bfc08242dac3b2e31fba2fae08e4378e1c56865
Fabi <frontfabi@gmail.com>
2022-03-07 11:25:31 -0300

    [fix] next.config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,8 +3,10 @@
  */
 const nextConfig = {
   images: {
-    loader: "default",
-    domains: ["localhost"],
+    loader: "cloudinary",
+    domains: ["localhost", "res.cloudinary.com"],
+    deviceSizes: [300, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
 }
 
